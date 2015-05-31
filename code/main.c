2bd1f9dcfc5b51f116b2eeb8f9aa9a5fa127ee56
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2015-05-31 16:03:04 +0100

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "zelib.c"
+#include "distribui.h"
+#include "distribui.c"
 
 int main(int argc, char *argv[]){
-    _entrelinhas('-', "hello world\n");
-    puts("puts");
+    Pessoa pessoa1 = pessoa("Ze", 49);
+    Pessoa pessoa2 = pessoa("Ana", 28);
+
+    _entrelinhas('-', "Pessoa\n");
+
+    //puts(pessoa1.nome);
     putz("putz BLUE", BLUE);
-    _printf(MAGENTA, "Hello %d:%s\n", MAGENTA, "MAGENTA");
+    printz(MAGENTA, "Pessoa #%d: %s, %d\n", pessoa1.id, pessoa1.nome, pessoa1.idade);
+    printz(CYAN, "Pessoa #%d: %s, %d\n", pessoa2.id, pessoa2.nome, pessoa2.idade);
     pause("press enter to continue");
+
     die("bye");
+
     exit(EXIT_SUCCESS);
 }
