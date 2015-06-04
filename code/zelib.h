d5b3dcfb36f77b7ab05d3ccb39481031fa646067
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2015-06-04 04:54:02 +0100

    Add zelib.h

---
+++ code/zelib.h
@@ -0,0 +1,45 @@
+#ifndef ZLIB
+#define ZLIB
+
+#define TRUE 1
+#define true 1
+#define FALSE 0
+#define false 0
+
+// macro para não ser necessário casting ou referencia do ponteiro
+#define zeFree(p) _zeFree((void **)&(p));
+void _zeFree(void **);
+
+typedef enum _cores {
+    BLACK = 0,
+    BLUE_FADE,// = FOREGROUND_BLUE,
+    GREEN_FADE,// = FOREGROUND_GREEN,
+    CYAN_FADE,// = FOREGROUND_GREEN | FOREGROUND_BLUE,
+    RED_FADE,// = FOREGROUND_RED,
+    MAGENTA_FADE,// = FOREGROUND_RED | FOREGROUND_BLUE,
+    YELLOW_FADE,// = FOREGROUND_RED | FOREGROUND_GREEN,
+    WHITE_FADE,// = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+    GRAY,// = FOREGROUND_INTENSITY,
+    BLUE,// = FOREGROUND_INTENSITY | FOREGROUND_BLUE,
+    GREEN,// = FOREGROUND_INTENSITY | FOREGROUND_GREEN,
+    CYAN,// = FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE,
+    RED,// = FOREGROUND_INTENSITY | FOREGROUND_RED,
+    MAGENTA,// = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE,
+    YELLOW,// = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN,
+    WHITE// = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
+} Cores;
+
+void die(char *);
+void _linha(char);
+void zentrelinhas(char, char *, ...);
+
+void putz(char *, int);
+void printz(int, char *, ...);
+void _printf(int, char *, ...);
+void pause(char *);
+void halt(char *);
+char *trimstr(char *, int, int);
+char *substr(char *, int, int);
+char *caps(char []);
+
+#endif
