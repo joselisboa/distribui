5796e55531530bc72e141fcb6384513df4112017
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2015-06-08 22:10:15 +0100

    Update util

--- code/util.c
+++ code/util.c
@@ -5,28 +5,29 @@
 
 void Util(){
     int opt;
+    char *nome = "Utilitarios";
 
     do{
         // cabeçalho
-        system("cls");
-        zenter("Utilitarios");
+        zenter_cls(nome);
 
-        switch(opt = option(UTIL_OPTIONS, UTIL_OPTIONS_N)){
+        switch(opt = optionz(UTIL_OPTIONS, UTIL_OPTIONS_N)){
         case 1: // Cores
+            zenter_cls(nome);
             _cores();
             break;
         case 2: // Caracteres
+            zenter_cls(nome);
             _caracteres();
             break;
         case 3:
             Testes();
             break;
         case 4:
-            return;
+        case 0: return;
         default:
-            printz(11, "[%s]\n", DADOS_OPTIONS[opt-1]);
-            putz("NAO IMPLEMENTADO", 12);
-            pause("premir qualquer tecla para continuar");
+            zenter_cls(nome);
+            Nimplementado(UTIL_OPTIONS[opt-1]);
         }
     } while(TRUE);
 }
