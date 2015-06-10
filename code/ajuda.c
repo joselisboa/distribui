320d9f052bb89edefbd21db23e5258dbafc084ee
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2015-06-10 08:29:05 +0100

    Update ajuda.c

--- code/ajuda.c
+++ code/ajuda.c
@@ -11,10 +11,10 @@ void Ajuda(){
         zenter_cls(AJUDA_NOME);
         opt = optionz(AJUDA_OPTIONS, AJUDA_N);
         switch(opt) {
-        //case 1: ajuda_regras();
-        //break;
-        //case 2: ajuda_comandos();
-        //break;
+        case 1: ajuda_regras();
+        break;
+        case 2: ajuda_comandos();
+        break;
         case 3: ajuda_ajuda();
         break;
         case 4:// Voltar
@@ -26,11 +26,35 @@ void Ajuda(){
 
 // mostra as regras do jogo
 void ajuda_regras(){
+    char buffer[255] = {'\0'};
+    FILE *fd = fopen("regras.txt", "r");
+    zenter_cls(AJUDA_NOME);
+    putz("[Regras]", 11);
+    
+    if(fd == NULL) return;
+    while(fgets(buffer, 255, fd) != NULL) {
+        if(buffer[0] == ' ') printz(7, "%s", buffer);
+        else printz(15, "%s", buffer);
+    }
+    fclose(fd);
+    pausa();
 }
 
 // mostra os comandos do jogo
 void ajuda_comandos(){
-}
+    char buffer[255] = {'\0'};
+    FILE *fd = fopen("comandos.txt", "r");
+    zenter_cls(AJUDA_NOME);
+    putz("[Comandos]", 11);
+    
+    if(fd == NULL) return;
+    while(fgets(buffer, 255, fd) != NULL) {
+        if(buffer[0] == ' ') printz(7, "%s", buffer);
+        else printz(15, "%s", buffer);
+    }
+    fclose(fd);
+    pausa();
+ }
 
 void ajuda_ajuda() {
     zenter_cls(AJUDA_NOME);
